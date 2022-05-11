8a61e2d04b7f03a013188b563873a1e2762ffbaf
zhtmahthgus <68368589+zhtmahthgus@users.noreply.github.com>
2022-05-11 22:55:13 +0900

    5.11 기능 추가

--- geulnoon/Word/test01.py
+++ geulnoon/Word/test01.py
@@ -5,14 +5,19 @@ from .W2V_word import W2V
 
 # 콤마, 괄호 삭제
 def REMOVE(st):
-    row = st
-    row2 = ' '.join(s for s in row)
-    remove = "'}"
+    row = ' '.join(s for s in st)
+    remove = "}"
     for x in range(len(remove)):
-        row3 = row2.replace(remove[x],"")
-    row4 = row3.replace("'","")
-    return row4
-
+      row1 = row.replace(remove[x],"")
+    row2 = row1.replace("'","")
+    row3 = row2.split('.')
+    strip_li = []
+    for i in row3:
+        i = i.strip()
+        if i:
+          strip_li.append(i)
+        
+    return strip_li
 
  #W2V 어휘들 뜻
 def W2V_MEAN(w2v_word):
@@ -49,7 +54,7 @@ def t01(word):
 
 
     w2v_mean = W2V_MEAN(w2v_word)
-    
+
     choice = { name:value for name, value in zip(w2v_word, w2v_mean) }
     choice[word] = mean
 
